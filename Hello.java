ce5764d919b0d653f948efee99721374c538e501
gekiiMei <86844554+gekiiMei@users.noreply.github.com>
2023-12-22 16:48:35 +0800

    Updated 2

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
         String[] members = {
             "Harley"
         };
-        System.out.println("Hello world!");
+        System.out.println("Hi world!");
         System.out.println("Members: ");
         for (String s : members) {
             System.out.println(s);
