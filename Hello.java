dbb954b648f2467d56fcc42117e810d84711f17b
gekiiMei <gekimeshi@gmail.com>
2023-12-22 16:46:13 +0800

    Updated main (added members array)

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
+        String[] members = {
+            "Harley"
+        };
         System.out.println("Hello world!");
+        System.out.println("Members: ");
+        for (String s : members) {
+            System.out.println(s);
+        }
     }
 }
